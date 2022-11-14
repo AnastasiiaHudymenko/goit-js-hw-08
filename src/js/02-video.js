56d78fd925236a0cedd6f9d6876655763a531e86
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2022-11-14 15:25:15 +0200

    add solution task-1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,7 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const currentTime = 'videoplayer-current-time';
+player.on('timeupdate', function () {});
