522d0a14434daafe0563f0c8bdcde4145f452114
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2022-12-19 15:04:40 +0200

    fix feedback form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,17 +4,17 @@ const refs = {
   form: document.querySelector('.feedback-form'),
   inputEmail: document.querySelector('input'),
   textares: document.querySelector('textarea'),
-  btnSubmit: document.querySelector('button'),
 };
 const FORM_STATS = 'feedback-form-state';
-const feedbackForm = {};
+const feedbackForm = JSON.parse(localStorage.getItem(FORM_STATS)) || {};
+
 populateFormInput();
 
 refs.form.addEventListener('input', throttle(onForminput, 500));
 refs.form.addEventListener('submit', onSubmitForm);
 
-function onForminput(e) {
-  feedbackForm[e.target.name] = e.target.value;
+function onForminput({ target }) {
+  feedbackForm[target.name] = target.value;
 
   localStorage.setItem(FORM_STATS, JSON.stringify(feedbackForm));
 }
@@ -28,9 +28,49 @@ function onSubmitForm(e) {
 
 function populateFormInput() {
   const saveDateForm = JSON.parse(localStorage.getItem(FORM_STATS));
-
   if (saveDateForm) {
-    refs.inputEmail.value = saveDateForm.email;
-    refs.textares.value = saveDateForm.message;
+    if (saveDateForm.email) {
+      refs.inputEmail.value = saveDateForm.email;
+    }
+    if (saveDateForm.message) {
+      refs.textares.value = saveDateForm.message;
+    }
   }
 }
+
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   inputEmail: document.querySelector('input'),
+//   textarea: document.querySelector('textarea'),
+// };
+
+// const CURRENT_KEY = 'feedback-form-state';
+// const formDate = {};
+
+// searchLocalStorageKey();
+// refs.form.addEventListener('input', throttle(onInputForm, 500));
+// refs.form.addEventListener('submit', onSubmitForm);
+
+// function onInputForm({ target }) {
+//   formDate[target.name] = target.value;
+//   localStorage.setItem(CURRENT_KEY, JSON.stringify(formDate));
+// }
+
+// function onSubmitForm(evt) {
+//   evt.preventDefault();
+//   console.log(JSON.parse(localStorage.getItem(CURRENT_KEY)));
+//   refs.form.reset();
+//   localStorage.removeItem(CURRENT_KEY);
+// }
+
+// function searchLocalStorageKey() {
+//   if (localStorage.getItem(CURRENT_KEY)) {
+//     const formData = JSON.parse(localStorage.getItem(CURRENT_KEY));
+//     if (formData.email) {
+//       refs.inputEmail.value = formData.email;
+//     }
+//     if (formData.message) {
+//       refs.textarea.value = formData.message;
+//     }
+//   }
+// }
